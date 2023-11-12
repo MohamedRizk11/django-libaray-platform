9bff92a5af51b0d3f509dc47eb23a96cf6971c26
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-11-13 01:10:04 +0200

    add models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,31 @@
+from datetime import date
+from django.utils import timezone
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name=models.CharField(max_length=50)
+    birth_date=models.DateField()
+    biography=models.TextField(max_length=1000)
+    
+
+
+
+class Book(models.Model):
+    title =models.CharField(max_length=100)
+    author=models.ForeignKey('Author',on_delete=models.CASCADE,related_name='book-author')
+    publication_date=models.DateTimeField(default=timezone.now)
+    price=models.IntegerField(null=True,blank=True)
+
+class Review(models.Model):
+    book=models.ForeignKey(Book,on_delete=models.CASCADE,related_name='review-book')
+    reviewer_name= models.CharField(max_length=50)
+    content =models.TextField(max_length=1000)
+    rating_choices = [(i, str(i)) for i in range(6)]  # Choices from 0 to 5
+    rating = models.IntegerField(choices=rating_choices, default=0)
+
+
+
