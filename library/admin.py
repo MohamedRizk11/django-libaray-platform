122981497d9af72c222ead5aff7a249b3f989bd7
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-11-13 13:45:52 +0200

    add search && filter

--- library/admin.py
+++ library/admin.py
@@ -5,12 +5,14 @@ from .models import Book,Author,Review
 
 
 
+class bookadmin(admin.ModelAdmin):
+    list_display=['title','author','publication_date','price']
+    search_fields=['title','author','price']
+    list_filter=['price','publication_date']
 
 
 
 
-
-
-admin.site.register(Book)
+admin.site.register(Book,bookadmin)
 admin.site.register(Author)
 admin.site.register(Review)
